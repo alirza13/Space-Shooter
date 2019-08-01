5a066207e65b32288e4f39504d2239756b6f28fe
alirza13 <33494868+alirza13@users.noreply.github.com>
2019-08-01 16:07:38 +0300

    Scrolling Background added

---
+++ Space Shooter/Assets/Scripts/Background/BGMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMovement : MonoBehaviour
+{
+    public float scrollSpeed = 0.12f;
+    private MeshRenderer meshRenderer;
+    private string MainTexture = "_MainTex";
+    // Start is called before the first frame update
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scroll();
+    }
+
+    void Scroll()
+    {
+        Vector2 offset = meshRenderer.sharedMaterial.GetTextureOffset(MainTexture);
+        offset.y -= Time.deltaTime * scrollSpeed;
+        meshRenderer.sharedMaterial.SetTextureOffset(MainTexture,offset);
+    }
+}
